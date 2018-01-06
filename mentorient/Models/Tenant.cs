5bb49bc971ba13b366862ca606f9c314eeb3bd3d
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-06 11:03:56 -0600

    add FirstName Prop to Tenant model

--- mentorient/Models/Tenant.cs
+++ mentorient/Models/Tenant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace mentorient.Models
     {
         public int Id { get; set; }
 
+        public string FirstName { get; set; }
 
     }
 }
