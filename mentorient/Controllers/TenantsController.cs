2f43870d3213af504983b88e4633d8ec289e179e
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-06 11:14:10 -0600

    add tenant view models

--- mentorient/Controllers/TenantsController.cs
+++ mentorient/Controllers/TenantsController.cs
@@ -10,6 +10,7 @@ namespace mentorient.Controllers
     {
         public IActionResult Index()
         {
+            
             return View();
         }
     }
---
+++ mentorient/Models/ViewModels/TenantsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mentorient.Models.ViewModels
+{
+    public class TenantsViewModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+    }
+}
