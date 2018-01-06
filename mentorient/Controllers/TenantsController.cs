829f8a5b27bb5ab5365dd3ad4039b4d0c0df9926
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-06 11:29:40 -0600

    pull list of tenants from db and show list

--- mentorient/Controllers/TenantsController.cs
+++ mentorient/Controllers/TenantsController.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using mentorient.Data;
+using mentorient.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
 
 namespace mentorient.Controllers
 {
     public class TenantsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public TenantsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var tenants = _context.Tenants.ToList();
             
-            return View();
+            return View(tenants);
         }
     }
 }
