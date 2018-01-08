9909581739e2b0f7439e2411f98019c1502b38c7
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-08 12:44:55 -0600

    AllowAnonymous on Home controller

--- mentorient/Controllers/HomeController.cs
+++ mentorient/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mentorient.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace mentorient.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public IActionResult Index()
