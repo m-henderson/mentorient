2f6dc429aa9774adf48f5a639cee005973ad796a
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-09 19:01:19 -0600

    update forms to save new tenant data

---
+++ mentorient/Data/Migrations/20180110005932_AddTenantProps.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace mentorient.Data.Migrations
+{
+    public partial class AddTenantProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Tenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Tenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Tenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Tenants",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Tenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Tenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Tenants",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Tenants");
+        }
+    }
+}
