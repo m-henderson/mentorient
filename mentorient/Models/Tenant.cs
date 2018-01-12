2f4d51f872b7342603b203750848918e0ef2f1ce
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-12 14:10:00 -0600

    make ttenant fields required

--- mentorient/Models/Tenant.cs
+++ mentorient/Models/Tenant.cs
@@ -9,15 +9,31 @@ namespace mentorient.Models
     public class Tenant
     {
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
         [Phone]
         public int PhoneNumber { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
         public string Email { get; set; }
+
+        [Required]
         public DateTime DateOfBirth { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string ZipCode { get; set; }
+
+        [Required]
         public string State { get; set; }
         public string Country { get; set; }
     }
