e0bb16c4d8c54fc62d57ccffa522703635eb5919
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-16 18:22:59 -0600

    add migrations for model change

---
+++ mentorient/Data/Migrations/20180117001734_ChangePhoneNumToString.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace mentorient.Data.Migrations
+{
+    public partial class ChangePhoneNumToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Tenants",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Tenants",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
