13d7e985c21e11cc2cc5cb0f5b9cc04264044cab
drazenp <drazenp86@gmail.com>
2018-01-23 20:28:48 +0000

    Added first last name on register form. Added migration #73

--- mentorient/Controllers/AccountController.cs
+++ mentorient/Controllers/AccountController.cs
@@ -220,7 +220,13 @@ namespace mentorient.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- mentorient/Data/Migrations/20180123012117_AddFirstLastName.Designer.cs
+++ mentorient/Data/Migrations/20180123012117_AddFirstLastName.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace mentorient.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180123012117_AddFirstLastName")]
+    partial class AddFirstLastName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
