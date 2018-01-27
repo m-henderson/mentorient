fdfeceede81bd1cd0715a7898c4de46572faf06b
Michael Henderson <mvhenderson.4@icloud.com>
2018-01-26 23:39:58 -0600

    add migration

--- mentorient/Data/Migrations/20180127053856_AddContactsToApplicationUser.Designer.cs
+++ mentorient/Data/Migrations/20180127053856_AddContactsToApplicationUser.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace mentorient.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180127053856_AddContactsToApplicationUser")]
+    partial class AddContactsToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ mentorient/Data/Migrations/20180127053856_AddContactsToApplicationUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace mentorient.Data.Migrations
+{
+    public partial class AddContactsToApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Contacts",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_ApplicationUserId",
+                table: "Contacts",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_AspNetUsers_ApplicationUserId",
+                table: "Contacts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_AspNetUsers_ApplicationUserId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_ApplicationUserId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Contacts");
+        }
+    }
+}
