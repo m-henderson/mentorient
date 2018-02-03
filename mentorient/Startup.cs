6cde8a90bffff9285a4228e692916457bbe112bc
Michael Henderson <mvhenderson.4@icloud.com>
2018-02-03 03:50:27 -0600

    update ConfigureServices to require ea confirmed email

--- mentorient/Startup.cs
+++ mentorient/Startup.cs
@@ -31,9 +31,13 @@ namespace mentorient
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+                {
+                    config.SignIn.RequireConfirmedEmail = true;
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
