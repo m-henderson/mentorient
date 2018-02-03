4f16d12ebe869cadaba3ce43f58ec3f73da0720e
Michael Henderson <mvhenderson.4@icloud.com>
2018-02-03 05:54:19 -0600

    setup sendgrid api key

--- mentorient/Services/AuthMessageSenderOptions.cs
+++ mentorient/Services/AuthMessageSenderOptions.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.AzureAppServices.Internal;
 
 namespace mentorient.Services
 {
     public class AuthMessageSenderOptions
     {
+        
+
         public string SendGridUser { get; set; }
+
         public string SendGridKey { get; set; }
     }
 }
