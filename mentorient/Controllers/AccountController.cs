7f34ae7de10391be4c0b58930c278b3d0d0321b9
Michael Henderson <mvhenderson.4@icloud.com>
2018-02-03 04:35:03 -0600

    require user to authenticate after registration

--- mentorient/Controllers/AccountController.cs
+++ mentorient/Controllers/AccountController.cs
@@ -66,6 +66,17 @@ namespace mentorient.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
+                // Require the user to have a confirmed email before they can log on.
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError(string.Empty,
+                            "You must have a confirmed email to log in.");
+                        return View(model);
+                    }
+                }
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
