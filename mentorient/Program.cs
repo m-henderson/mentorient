32407dcd9a9484cc1e3852036d0a46617df2703b
Michael Henderson <mvhenderson.4@icloud.com>
2018-02-16 20:19:37 -0600

    set startup port

--- mentorient/Program.cs
+++ mentorient/Program.cs
@@ -20,6 +20,7 @@ namespace mentorient
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:32778/")
                 .Build();
     }
 }
